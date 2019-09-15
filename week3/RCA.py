a1c11990eb5380463243d4edf6fd02286427ff06
Edi Begovic <edibegovic@gmail.com>
2019-09-16 01:40:02 +0200

    RCA plots added

--- week3/RCA.py
+++ week3/RCA.py
@@ -1,5 +1,9 @@
+
 import numpy as np
 from numpy import genfromtxt
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 bodyfat_data = genfromtxt('bodyfat.txt', delimiter=',')
 N = len(bodyfat_data[:, 0])
@@ -17,8 +21,20 @@ C = np.dot(np.transpose(X), X)/N
 e_val = np.linalg.eig(C)[0]
 e_vec = np.linalg.eig(C)[1]
 
-# Eigenvectors sorted
+# Eigenvectors sorted (principal components)
 W = np.flip(e_vec, 0)
 
 
+Z = np.dot(np.transpose(W), np.transpose(X))
+
+# K = 2 projection
+plt.scatter(Z[0], Z[1], color='red', s=15)
+
+# K = 1 projection
+plt.scatter(Z[0], [0]*N, color='green', s=15)
+
+# OG values
+plt.scatter(fat, abdom, color='blue', s=15)
+
+plt.show()
 
