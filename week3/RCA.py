1e4729e52a3eb139106b354b156886d33fbce9e1
Edi Begovic <edibegovic@gmail.com>
2019-09-16 13:05:23 +0200

    W3.3 done

--- week3/RCA.py
+++ week3/RCA.py
@@ -24,20 +24,32 @@ e_vec = np.linalg.eig(C)[1]
 # Eigenvectors sorted (principal components)
 W = e_vec
 
-# OG points, mean-shifted points and PC
+# [PLOTS] OG points, mean-shifted points and PC
 plt.scatter(fat, abdom, color='blue', s=15)
 plt.scatter(np.transpose(X)[0], np.transpose(X)[1], color='red', s=15)
 abline(1.366, [0,0])
 abline(-0.733, [0,0])
 plt.show()
 
-# Projection
+# Projection of X on W
 Z = np.dot(np.transpose(W), np.transpose(X))
-# K = 2 projection
-plt.scatter(Z[0], Z[1], color='red', s=15)
-# K = 1 projection
-plt.scatter(Z[0], [0]*N, color='green', s=15)
+
+# [PLOTS] OG points, K=1, K=2
+plt.scatter(fat, abdom, color='blue', s=15)
+plt.scatter(Z[1], Z[0], color='red', s=15)
+plt.scatter(Z[1], [0]*N, color='green', s=15)
 plt.show()
+
+# 1D reconstruction of X
+Z_1D = np.array([Z[1], [0]*N])
+
+X_approx = np.dot(np.transpose(W), Z_1D)
+plt.scatter(fat, abdom, color='blue', s=15)
+plt.scatter(X_approx[1]+m[0], X_approx[0]+m[1], color='red', s=15)
+plt.show()
+
+
+
 
 
 
