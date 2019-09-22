66be71772b49eb3300f4efcf55bbd4808fc52f64
Edi Begovic <edibegovic@gmail.com>
2019-09-22 14:59:23 +0200

    K means done

--- week4/exercise43.py
+++ week4/exercise43.py
@@ -1,6 +1,7 @@
 
-import numpy as np
+import cv2
 import random
+import numpy as np
 from numpy import linalg as la
 from random import uniform
 from sklearn.metrics import confusion_matrix
@@ -12,10 +13,10 @@ iris_labels = np.genfromtxt('iris_labels.txt', delimiter=',')
 def k_means(x, k):
     # initial k reference vectors
     m = random.sample(list(x), k)
-    g_class = lambda l, b, c: [l[idx] for idx, val in enumerate(b) if val == c]
+    g_class = lambda l, b, c: np.array([l[idx] for idx, val in enumerate(b) if val == c])
     for _ in range(5):
         b = [min([(j, la.norm(i-l)) for j, l in enumerate(m)], key = lambda t: t[1])[0] for i in x]
-        m = [sum(g_class(x, b, i))/len(g_class(x, b, i)) for i, _ in enumerate(m)]
+        m = [(g_class(x, b, i)).sum(axis=0)/len(g_class(x, b, i)) for i, _ in enumerate(m)]
     return (b, m)
 
 def k_means_optimal(x, k, n = 20):
@@ -48,54 +49,23 @@ get_confusion_matrix(lim_v([1,2]))
 get_confusion_matrix(lim_v([1,3]))
 get_confusion_matrix(lim_v([2,3]))
 
-# The third class always seem to be confused, while the 
+# The third class always seems to be confused, while the 
 # first class is always correctly classified.
 #
 # [1, 2] is basically as good as using all values.
 # [2, 3] and [3, 4] are better than using all values.
 
 
+# EXERCISE E (image)
+def k_means_image(path, k):
+    flatten = lambda l: [item for sublist in l for item in sublist]
+    img_arr = cv2.imread(path)
+    height, width, depth = img_arr.shape
+    k = np.array(flatten([img_arr[x, :, :] for x in range(img_arr.shape[0])]))
+    _, ref_col = k_means(k[:, :], 3)
+    applied_ref_cols = ([min([(l, la.norm(i-l)) for j, l in enumerate(ref_col)], key = lambda t: t[1])[0] for i in k])
+    re_shaped = np.array([[applied_ref_cols.pop(0) for _ in range(width)] for x in range(height)])
+    cv2.imwrite('k_means_imgage.jpg', re_shaped)
 
-# BONUS
-def k_means(x, k):
-    # initial k reference vectors
-    m = random.sample(list(x), k)
-    print(m)
-    g_class = lambda l, b, c: [l[idx] for idx, val in enumerate(b) if val == c]
-    for _ in range(1):
-        b = [min([(j, la.norm(i-l)) for j, l in enumerate(m)], key = lambda t: t[1])[0] for i in x]
-        # with open("test.txt", "a") as myfile:
-            # myfile.write(str(sum(g_class(x,b,i))))
-            # print("written")
-        # m = [sum(g_class(x, b, i))/len(g_class(x, b, i)) for i, _ in enumerate(m)]
-        # print("len b:", len(b))
-        for idx, _ in enumerate(m):
-            print("--- NY M----")
-            f = [x[idd] for idd, vaa in enumerate(b) if idx == vaa]
-            print(f)
-            print("spæe")
-            print((f[0])+(f[1]))
-            # boks1 = []
-            # for idx2, val in enumerate(b):
-            #     if(idx == val):
-            #         print(x[idx2])
-            #         # boks1.append(np.array(x[idx2]))
-            #         boks1.append(x[idx2])
-            # print("MAGIC PRINT: ", ((boks1)))
-    return "test"
-
-import cv2
-m = cv2.imread("gt.jpg")
-m.shape
-
-flatten = lambda l: [item for sublist in l for item in sublist]
-k = np.array(flatten([m[x, :, :] for x in range(m.shape[0])]))
-k.shape
-
-k_means(k[:20, :], 3)
-
-cv2.imwrite('color_img.jpg', m)
-
-([np.array([178, 127, 1]), np.array([177, 126, 0]), np.array([176, 125, 0])])[0].shape
-
-
+# Takes a few minues
+k_means_image("tree.jpg", k=3)
