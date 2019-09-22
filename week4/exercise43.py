b88c449ce3f25c6d4bb4738f6c91a6ded9a12be2
Edi Begovic <edibegovic@gmail.com>
2019-09-22 14:01:23 +0200

    temp test

--- week4/exercise43.py
+++ week4/exercise43.py
@@ -11,7 +11,7 @@ iris_labels = np.genfromtxt('iris_labels.txt', delimiter=',')
 # EXERCISE A
 def k_means(x, k):
     # initial k reference vectors
-    m = random.sample(list(x), k) 
+    m = random.sample(list(x), k)
     g_class = lambda l, b, c: [l[idx] for idx, val in enumerate(b) if val == c]
     for _ in range(5):
         b = [min([(j, la.norm(i-l)) for j, l in enumerate(m)], key = lambda t: t[1])[0] for i in x]
@@ -57,6 +57,45 @@ get_confusion_matrix(lim_v([2,3]))
 
 
 # BONUS
+def k_means(x, k):
+    # initial k reference vectors
+    m = random.sample(list(x), k)
+    print(m)
+    g_class = lambda l, b, c: [l[idx] for idx, val in enumerate(b) if val == c]
+    for _ in range(1):
+        b = [min([(j, la.norm(i-l)) for j, l in enumerate(m)], key = lambda t: t[1])[0] for i in x]
+        # with open("test.txt", "a") as myfile:
+            # myfile.write(str(sum(g_class(x,b,i))))
+            # print("written")
+        # m = [sum(g_class(x, b, i))/len(g_class(x, b, i)) for i, _ in enumerate(m)]
+        # print("len b:", len(b))
+        for idx, _ in enumerate(m):
+            print("--- NY M----")
+            f = [x[idd] for idd, vaa in enumerate(b) if idx == vaa]
+            print(f)
+            print("spæe")
+            print((f[0])+(f[1]))
+            # boks1 = []
+            # for idx2, val in enumerate(b):
+            #     if(idx == val):
+            #         print(x[idx2])
+            #         # boks1.append(np.array(x[idx2]))
+            #         boks1.append(x[idx2])
+            # print("MAGIC PRINT: ", ((boks1)))
+    return "test"
+
 import cv2
+m = cv2.imread("gt.jpg")
+m.shape
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+k = np.array(flatten([m[x, :, :] for x in range(m.shape[0])]))
+k.shape
+
+k_means(k[:20, :], 3)
+
+cv2.imwrite('color_img.jpg', m)
+
+([np.array([178, 127, 1]), np.array([177, 126, 0]), np.array([176, 125, 0])])[0].shape
 
 
